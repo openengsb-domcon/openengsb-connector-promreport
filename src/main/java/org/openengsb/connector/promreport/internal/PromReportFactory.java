97abc6700228702f8557ece29d910d67d4380bdc
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-11-30 14:28:31 +0100

    [OPENENGSB-2329] used wrong issue id before
    This reverts commit 1d6ff66e2b9b98bfbf6a0710d11ce18c39bae7f2.

--- src/main/java/org/openengsb/connector/promreport/internal/PromReportFactory.java
+++ src/main/java/org/openengsb/connector/promreport/internal/PromReportFactory.java
@@ -18,7 +18,6 @@ package org.openengsb.connector.promreport.internal;
 import java.util.Map;
 
 import org.openengsb.core.api.Connector;
-import org.openengsb.core.api.ekb.EngineeringKnowledgeBaseService;
 import org.openengsb.core.common.AbstractConnectorInstanceFactory;
 import org.openengsb.domain.report.ReportDomainEvents;
 import org.openengsb.domain.report.common.ReportStoreFactory;
@@ -31,8 +30,6 @@ public class PromReportFactory extends AbstractConnectorInstanceFactory<PromRepo
     
     @SuppressWarnings("unused")
     private ReportDomainEvents reportEvents;
-    @SuppressWarnings("unused")
-    private EngineeringKnowledgeBaseService ekbService;
 
     @Override
     public Connector createNewInstance(String id) {
@@ -58,10 +55,6 @@ public class PromReportFactory extends AbstractConnectorInstanceFactory<PromRepo
 
     public void setEventTransformator(EventTransformator eventTransformator) {
         this.eventTransformator = eventTransformator;
-    }
-    
-    public void setEkbService(EngineeringKnowledgeBaseService ekbService) {
-        this.ekbService = ekbService;
     }
 
     public void setReportEvents(ReportDomainEvents reportEvents) {
